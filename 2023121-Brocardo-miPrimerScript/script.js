7aab626f65de4a62c04bbaf7de9ee803621bea7d
BrocardoSofia <sofia.brocardo@hotmail.com>
2023-12-01 12:46:21 -0300

    ejercicios javascript

---
+++ 2023121-Brocardo-miPrimerScript/script.js
@@ -0,0 +1,61 @@
+
+function mensajeAlert()
+{
+    let mensaje ="Hola mundo por variable";
+
+    alert(mensaje);
+}
+
+function cantidadDeCaracteres()
+{
+    let userInput = prompt("Ingrese una palabra");
+
+    return "La palabra: "+userInput+" tiene "+userInput.length+" caracteres";
+}
+
+function calculadoraDeEdad()
+{
+    let userEdad = prompt("Ingrese su edad");
+    let dias = parseInt(userEdad) * 365;
+
+    return "Tienes "+ dias + " en la tierra";
+}
+
+function sumaDeValores()
+{
+    let num1 = parseInt(prompt("Ingrese el primer numero: "));
+    let num2 = parseInt(prompt("Ingrese el segundo numero: "));
+    let resultado = num1 + num2;
+
+    return resultado;
+}
+
+function calculadorAbastecimientoDePorVida()
+{
+    let userEdad = parseInt(prompt("Ingrese su edad"));
+    let userEdadMax = parseInt(prompt("Ingrese su edad maxima"));
+    let snacksPorDia = parseInt(prompt("Snacks que comes por dia: "));
+    let snackPrecio = parseFloat(prompt("Ingrese el precio por unidad de snack: "));
+    let diasRestantes = (userEdadMax-userEdad)*365;
+    let snacksRestantes = diasRestantes * snacksPorDia;
+
+    alert("Necesitarás "+snacksRestantes+" snacks para que te alcancen hasta los "+userEdadMax+" años.");
+    return "Gastaras $"+snacksRestantes*snackPrecio+" en "+snacksRestantes+" snacks";
+}
+
+function dineroVacaciones()
+{
+    let diasViaje = parseInt(prompt("Ingrese los dias que va a estar de viaje: "));
+    let presupuesto = parseFloat(prompt("Ingrese su presupuesto maximo para el viaje: "));
+    let comidaCant = parseInt(prompt("Ingrese la cantidad de comidas que va a tener en todo su viaje "));
+    let gastoMaxPorComida = presupuesto/comidaCant;
+
+    alert("Podés gastar $"+gastoMaxPorComida.toFixed(2)+" en cada comida para que te alcance la plata durante los "+diasViaje+" días de viaje");
+}
+
+//mensajeAlert();
+//console.log(cantidadDeCaracteres());
+//console.log(calculadoraDeEdad());
+//console.log(sumaDeValores());
+//console.log(calculadorAbastecimientoDePorVida());
+dineroVacaciones();
