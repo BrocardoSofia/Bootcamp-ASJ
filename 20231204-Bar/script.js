97906089c6c02bac0abe67901fcdc1530b8c0110
BrocardoSofia <sofia.brocardo@hotmail.com>
2023-12-04 14:42:43 -0300

    ejercicio bar

--- 20231204-Bar/script.js
+++ 20231204-Bar/script.js
@@ -6,3 +6,139 @@ que pregunte si deja propina (sumar al total a pagar)
 en algun momento puede contar chistes (pedir contar chistes en cualquier momento)
 saludar a ingresar y al salir
 */
+
+let opcion = 0;
+let saludo = 0;
+let mesero = "Mesero";
+let comida;
+let factura = "FACTURA\n";
+let cuenta = 0;
+let mensajePedirComida = "Que lo disfrute!!";
+let propina = 0;
+let propinaPagar = 0.00;
+let contadorChiste = 0;
+
+//entra el cliente al bar
+alert("Bienvenido al bar!! (｡◕‿◕｡)"); //bienvenida al bar
+do {
+  opcion = parseInt(prompt("¿Que desea hacer?\n\n1. Saludar\n2. Ver la carta\n3. Pedir comida\n4. Pedir un chiste\n5. Pedir la cuenta"));
+
+  switch (opcion) {
+    case 1:
+        alert("Buen dia! (ʘ‿ʘ)╯");
+        if(saludo === 0)
+        {
+            alert(mesero+": 'Buen dia!' ⊂(◉‿◉)つ");
+            saludo = 1;
+        }
+        else
+        {
+            alert(mesero+": 'usted ya me saludo' (ㆆ _ ㆆ)");
+        }
+      break;
+
+    case 2:
+      //muestro el menu
+      alert(
+        "MENU: \n\n1. Cerveza ....... $1500 \n2. Papas ........... $5000\n3. Pizza ............. $3400 \n4. Rabas ........... $4000"
+      );
+      break;
+
+    case 3:
+        //muestro las opciones
+        comida = parseInt(prompt("Selecciones una de las comidas del menu: \n\n1. Cerveza  \n2. Papas \n3. Pizza  \n4. Rabas "));
+        switch(comida)
+        {
+            case 1:
+                factura+="\nCerveza ....... $1500";//agrego la comida a la factura
+                cuenta+=1500;//sumo a la cuenta final
+                break;
+
+            case 2:
+                factura+="\nPapas ........... $5000";//agrego la comida a la factura
+                cuenta+=5000;//sumo a la cuenta final
+                break;
+
+            case 3:
+                factura+="\nPizza ............. $3400";//agrego la comida a la factura
+                cuenta+=3400;//sumo a la cuenta final
+                break;
+
+            case 4:
+                factura+="\nRabas ........... $4000";//agrego la comida a la factura
+                cuenta+=4000;//sumo a la cuenta final
+                break;
+
+            default:
+                alert("Opcion no valida!");
+                break;
+        }
+        if(comida===1 || comida===2 || comida===3 || comida===4)
+        {
+            alert(mensajePedirComida);
+        }
+
+        break;
+
+    case 4:
+        //pedir chiste
+        switch(contadorChiste)
+        {
+            case 0:
+                alert("¿Qué le dice una barra de pan a otra? Te presento a una miga.?\n(≧︿≦)");
+                contadorChiste++;
+                break;
+            case 1:
+                alert("¿Sabes que el aguacate es 50% agua? \nSí, lo sé. Y el otro 50% cate.\n(≧︿≦)");
+                contadorChiste++;
+                break;
+            case 2:
+                alert("¿Qué le dice un limón policía a un limón sospechoso de un crimen?\n¿Ácido tú?\n(≧︿≦)");
+                contadorChiste++;
+                break;
+            case 3:
+                alert("¿Cuál es la fruta más explosiva y peligrosa? ¡La granada!\n(≧︿≦)");
+                contadorChiste++;
+                break;
+            case 4:
+                alert("¿Qué le dice un pollito a otro pollo al que odia?\n¡Caldito seas!\n(≧︿≦)");
+                contadorChiste++;
+                break;
+            case 5:
+                alert("Me quede sin chistes ( ͡° ʖ̯ ͡°)");
+                break;
+
+        }
+        break;
+
+    case 5:
+        //pide la cuenta
+        break;
+
+    default:
+        alert("Opcion no valida!");
+        break;
+  }
+} while (opcion != 5);
+
+//cuenta
+do{
+    alert("Cuenta!");
+    //muestro la factura y pregunto si quiere agregar propina
+    factura += "\n"+"Total: ............ $"+cuenta;
+    propina = prompt(factura+"\n\nDesea agregar propina? (s/n)").toLowerCase();
+
+    console.log(propina!='s');
+
+}while(propina!="s" && propina!="n");
+
+if(propina == "s")
+{
+    propinaPagar = parseFloat(prompt("Ingrese el monto para la propina:"));
+}
+
+//muestro la factura final:
+cuenta+=propinaPagar;
+alert(factura+"\n"+"Propina: ...... $"+propinaPagar+"\n\nFinal: ............ $"+cuenta);
+
+alert("Adios!!, muchas gracias por venir (｡◕‿◕｡)"); 
