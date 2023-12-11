83f93a550c91a4dff1239632c818b0b7ae68f462
BrocardoSofia <sofia.brocardo@hotmail.com>
2023-12-11 20:34:32 -0300

    weather final

--- 20231211-weather/script.js
+++ 20231211-weather/script.js
@@ -1,11 +1,12 @@
 function cargarCiudad(ciudad) {
-  fetch("https://api.openweathermap.org/data/2.5/weather?q="+ciudad+"&appid=dcec7df661b1e6b0edab51d796b7339c&units=metric")
+  fetch("https://api.openweathermap.org/data/2.5/weather?q="+ciudad+"&appid=dcec7df661b1e6b0edab51d796b7339c&units=metric&lang=es")
     .then((response) => response.json()) 
     .then((json) => {
         document.querySelector(".container").style.visibility = "visible";
         document.querySelector("#ciudad").textContent = json.name;
-        document.querySelector("#temperatura").textContent = json.main.temp;
+        document.querySelector("#temperatura").innerHTML = (json.main.temp).toFixed(1) + "<sup>°C</sup>";
         document.querySelector("#descripcion").textContent = json.weather[0].description;
+        document.querySelector("#wicon").src = "https://openweathermap.org/img/wn/"+json.weather[0].icon+"@2x.png";
         console.log(json);
     })
     .catch((error) => console.log("Ocurrió un error: " + error));
