c55c655cdb6dfc613a5b1992bff16ed1c0cf92e1
BrocardoSofia <sofia.brocardo@hotmail.com>
2023-12-12 15:50:56 -0300

    angular con modulos

--- 20231212-primerProyectoAngular/proyecto/src/app/app.component.ts
+++ 20231212-primerProyectoAngular/proyecto/src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MiComponenteComponent } from "./components/mi-componente/mi-componente.component";
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, MiComponenteComponent]
 })
 export class AppComponent {
   title = 'proyecto';
+  usuario = 'Sofia';
 }
