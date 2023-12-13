4aa9c51936fd8cdbeb7d49c55899bd5b9aebef3f
BrocardoSofia <sofia.brocardo@hotmail.com>
2023-12-13 17:03:19 -0300

    task list

--- ejerciciosAngular/exercises-app/src/app/components/to-do-list/to-do-list.component.ts
+++ ejerciciosAngular/exercises-app/src/app/components/to-do-list/to-do-list.component.ts
@@ -5,6 +5,33 @@ import { Component } from '@angular/core';
   templateUrl: './to-do-list.component.html',
   styleUrl: './to-do-list.component.css'
 })
-export class ToDoListComponent {
 
+export class ToDoListComponent {
+  task: string = "";
+  taskList: Task[] = [];
+
+  addTask(){
+    if(this.task !== "")
+    {
+      this.taskList.push(new Task(this.task, new Date()));
+      this.task = "";
+    }
+  }
+
+  removeTask(index: number){
+    this.taskList[index].selected=true;
+  }
+}
+
+class Task{
+	taskName: string;
+	date: Date;
+	selected: boolean;
+
+  constructor(taskName: string, date: Date)
+  {
+    this.taskName = taskName;
+    this.date = date;
+    this.selected = false;
+  }
 }
