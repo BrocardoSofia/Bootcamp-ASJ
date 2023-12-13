628c13f2bc66adcd7c9e30063dabed1bd312451c
BrocardoSofia <sofia.brocardo@hotmail.com>
2023-12-13 15:39:44 -0300

    base for angular proyect

---
+++ ejerciciosAngular/exercises-app/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AsideMenuComponent } from './components/aside-menu/aside-menu.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+import { ToDoListComponent } from './components/to-do-list/to-do-list.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AsideMenuComponent,
+    FooterComponent,
+    MainComponent,
+    ToDoListComponent,
+    NavBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
