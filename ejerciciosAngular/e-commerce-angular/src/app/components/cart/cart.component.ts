b7334af0cab2b06aca5d1cbb919bd114d2b85014
BrocardoSofia <sofia.brocardo@hotmail.com>
2023-12-20 16:45:55 -0300

    update

--- ejerciciosAngular/e-commerce-angular/src/app/components/cart/cart.component.ts
+++ ejerciciosAngular/e-commerce-angular/src/app/components/cart/cart.component.ts
@@ -34,6 +34,11 @@ export class CartComponent implements OnInit{
     
   }
 
+  changeImage(event: Event): void {
+    const imagen = event.target as HTMLImageElement;
+    imagen.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/832px-No-Image-Placeholder.svg.png';
+  }
+
   buyCart(){
     //llamar al servicio para comprar carrito
     this.api_service.buyCart();
