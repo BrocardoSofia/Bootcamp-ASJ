9599e665129e92f54f85445f3968aea23dd4f75c
BrocardoSofia <sofia.brocardo@hotmail.com>
2024-02-21 09:50:55 -0300

    update

--- React/mi-primera-app/src/App.js
+++ React/mi-primera-app/src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Componente1 from './components/Componente1';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>
+          Hola Mundo!
+        </h2>
+        <p>Primera App en React.</p>
+        <Componente1 texto="Otro texto... " color="Red"/>
+        <Componente1 texto="Hola, que tal?"/>
       </header>
     </div>
   );
---
+++ React/mi-primera-app/src/components/Componente1.js
@@ -0,0 +1,10 @@
+function Componente1({texto, color}){
+    return (
+        <>
+            <h1>Hola</h1>
+            <em style={{color:color}}>{texto}</em>
+        </>
+    );
+}
+
+export default Componente1;
